89c38977fc9ce33156774c920f7d8eae527e291d
ElysseCSimbulan <julianneelysse.simbulan@smc.pshs.edu.ph>
2026-03-03 00:57:36 +0800

    Add files via upload

---
+++ Simbulan_Q3_FA06.py
@@ -0,0 +1,10 @@
+student = {}
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+subject = input("Enter your favorite subject: ")
+
+print("\nStudent Record:")
+print("Name:", name)
+print("Age:", age)
+print("Favorite Subject:", subject)
